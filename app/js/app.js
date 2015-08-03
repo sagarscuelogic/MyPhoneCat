376c16a21560c9e08646e4a42b589b55200e9339
sagarscuelogic <sagar.sutaria@cuelogic.co.in>
2015-08-03 19:35:11 +0530

    Development till implmentation for usage of built-in  service to get data from remote server

---
+++ app/js/app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+/* App Module */
+var phonecatApp = angular.module('phonecatApp', []);
---
+++ app/js/controllers.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* Controllers */
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('phones/phones.json').success(function(data) {
+    $scope.phones = data;
+  });
+
+  $scope.orderProp = 'age';
+}]);
