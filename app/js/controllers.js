301fff03ef665251aec6b012f36e7a0ae81ade36
sagarscuelogic <sagar.sutaria@cuelogic.co.in>
2015-08-05 18:46:15 +0530

    Implemented routing, custom services, enabled RESTfull approach, created custom filter, added event handlers, used angular animations

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,16 +4,21 @@
 
 var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
-  function($scope, $http) {
-    $http.get('phones/phones.json').success(function(data) {
-      $scope.phones = data;
-    });
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http', 'Phone',
+    function ($scope, $http, Phone) {
+        $scope.phones = Phone.query();
 
-    $scope.orderProp = 'age';
-  }]);
+        $scope.orderProp = 'age';
+    }]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    $scope.phoneId = $routeParams.phoneId;
-  }]);
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+    function ($scope, $routeParams, $http) {
+        $http.get('phones/' + $routeParams.phoneId + '.json').success(function (data) {
+            $scope.phone = data;
+            $scope.mainImageUrl = data.images[0];
+        });
+
+        $scope.setImage = function (imageUrl) {
+            $scope.mainImageUrl = imageUrl;
+        };
+    }]);
